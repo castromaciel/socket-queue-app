95adc39199a4134ca791497376a513feca1a9902
castromaciel <castromaciel00@gmail.com>
2023-05-20 00:01:21 -0300

    feat: prepare desktop display

--- public/js/desktop.js
+++ public/js/desktop.js
@@ -1,3 +1,37 @@
+const desktopLabel = document.querySelector('h1')
+const serveButton = document.querySelector('button')
 
+const search = window.location.search
+const searchParams = new URLSearchParams(search)
 
-console.log('Escritorio HTML');
+if (!searchParams.has('desktop')) {
+  window.location = 'index.html'
+  throw new Error('Desktop is required')
+}
+
+const desktop = searchParams.get('desktop')
+desktopLabel.innerHTML = desktop
+
+const socket = io()
+
+socket.on('connect', () => {
+  serveButton.disabled = false
+})
+
+socket.on('disconnect', () => {
+  serveButton.disabled = true
+})
+
+socket.on('send-message', (payload) => {
+  console.log('From Server', payload)
+})
+
+socket.on('last-ticket', lastTicket => {
+  // newTicketLabel.innerText = lastTicket
+})
+
+serveButton.addEventListener('click', () => {
+  // socket.emit('next-ticket', null, (ticket) => {
+  //   newTicketLabel.innerText = ticket
+  // })
+})
